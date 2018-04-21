ba6a668aba70f9901dd6cba292aa449a60c4561a
Mohammad Mustafa <37963449+MoMustafa@users.noreply.github.com>
2018-04-20 21:53:26 -0500

    nothing - fix

--- ServerClientNetwork/src/MyFTP/MyClient.java
+++ ServerClientNetwork/src/MyFTP/MyClient.java
@@ -8,6 +8,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
+import java.net.InetAddress;
 import java.net.Socket;
 import java.security.NoSuchAlgorithmException;
 import java.util.Scanner;
@@ -26,7 +27,7 @@ public class MyClient extends Socket
 	private static BufferedReader br;
 	private static BufferedWriter bw;
 
-	// VARIABLES 
+	// VARIABLES
 	private static int port;
 	private static String message = null;
 	private static String filename = null;
@@ -34,6 +35,11 @@ public class MyClient extends Socket
 	private static String command = null;
 	private static String[] commandarr = null;
 
+	/*
+	 * Instantiates MyClient by using the user assigned port number and server
+	 * address. I/O Streams are also instantiated so that They are not
+	 * prematurely closed elsewhere in the code.
+	 */
 	public MyClient(String serveraddress, int port) throws IOException
 	{
 		System.out.println(
@@ -49,36 +55,83 @@ public class MyClient extends Socket
 		bw = new BufferedWriter(osw);
 	}
 
+	/*
+	 * Begins communication between server and client. On this client side, the
+	 * Client Socket initiates the communication, after which each side takes
+	 * turns writing replies. If any side sends "end" both sides begin to
+	 * terminate connection. If the client receives a message that begins with
+	 * "send" it exits communication mode And goes into sendfile() code.
+	 * 
+	 * If the server side sends a message that begins with "send" it exits
+	 * communication mode And goes into receivefile() mode. In both of those
+	 * cases, before exiting, we save the filename being requested or sent.
+	 * 
+	 * If the file does not exist, the client sends a reply saying so and waits
+	 * for the server to send a reply.
+	 */
 	private void communicate() throws IOException
 	{
+		boolean DNE = false;
+
 		while (true)
 		{
-			message = scanner.nextLine();
-			if (message.startsWith("send"))
+			if (!DNE)
 			{
-				String filename = (message.split(" ", 2))[1];
-				command = "receive " + filename;
+				message = scanner.nextLine();
+				if (message.startsWith("sendTCP"))
+				{
+					String filename = (message.split(" ", 2))[1];
+					command = "receiveTCP " + filename;
+				}
+				if (message.startsWith("sendUDP"))
+				{
+					String filename = (message.split(" ", 2))[1];
+					command = "receiveUDP " + filename;
+				}
+				bw.write(message + "\n");
+				bw.flush();
 			}
-			bw.write(message + "\n");
-			bw.flush();
-
+			DNE = false;
 			if (message.equals("end")) break;
 
 			if ((message = br.readLine()) != null)
 			{
 				if (message.startsWith("send"))
 				{
-					command = message;
-					bw.write("end\n");
-					bw.flush();
-					break;
+					File file = new File((message.split(" ", 2))[1]);
+					if (!file.exists())
+					{
+						System.out.println(
+								"CLIENT: Server said \"" + message + "\"");
+						System.out.println(
+								"CLIENT: Telling Server file does not exist.");
+						bw.write("File does not exist. Try again.\n");
+						bw.flush();
+						DNE = true;
+					}
+					else
+					{
+						command = message;
+						bw.write("end\n");
+						bw.flush();
+						break;
+					}
 				}
-				System.out.println("CLIENT: Server said \"" + message + "\"");
+				if (!DNE)
+					System.out
+							.println("CLIENT: Server said \"" + message + "\"");
 				if (message.equals("end")) break;
 			}
 		}
 	}
 
+	/*
+	 * Main method requests port number and server address from user and creates
+	 * a MyClient object. Then the client goes into communicate() mode. When the
+	 * client exits the communicate mode, we check the command string to
+	 * Determine whether we need to send or receive, and what the filename is We
+	 * send the socket and filename as parameters to the appropriate methods.
+	 */
 	public static void main(String args[])
 			throws IOException, ClassNotFoundException, NoSuchAlgorithmException
 	{
@@ -88,32 +141,36 @@ public class MyClient extends Socket
 		port = Integer.parseInt(scanner.nextLine());
 
 		MyClient testclient = new MyClient(serveraddress, port);
-                
+
 		testclient.communicate();
 		if (command != null)
 		{
-			commandarr = command.split(" ", 3);
+			commandarr = command.split(" ", 2);
 			filename = commandarr[1];
-                        String protocol = commandarr[2];
 
-			if ((commandarr[0]).equals("receive"))
+			if ((commandarr[0]).equals("receiveTCP")
+					|| (commandarr[0]).equals("receiveUDP"))
 			{
+				
 				System.out.println("CLIENT: Going to receive file.");
-				if(protocol.equals("TCP"))
-                                {
-                                    new TCP();
-                                    TCP.receivefileTCP(filename, client);
-                                    testclient.close();
-                                }
-                                else if(protocol.equals("UDP"))
-                                {
-                                    new UDP(port);
-                                  UDP.sendDataUDP(filename);
-                                    testclient.close();
-                                }
-				return;
+				if ((commandarr[0]).equals("receiveUDP"))
+				{
+					InetAddress address = InetAddress.getLocalHost();
+					new UDP(port, address);
+					UDP.receive(filename);
+					testclient.close();
+					return;
+				}
+				if ((commandarr[0]).equals("receiveTCP"))
+				{
+					new TCP();
+					TCP.receivefileTCP(filename, client);
+					testclient.close();
+					return;
+				}
 			}
-			if ((commandarr[0]).equals("send"))
+			if ((commandarr[0]).equals("sendTCP")
+					|| (commandarr[0]).equals("sendUDP"))
 			{
 				File file = new File(filename);
 				if (!file.exists())
@@ -126,12 +183,22 @@ public class MyClient extends Socket
 					return;
 				}
 				System.out.println("CLIENT: Got command to " + command);
-				new TCP();
-				TCP.sendfileTCP(filename, client);
-				os.flush();
-				client.close();
-				testclient.close();
-				return;
+
+				if ((commandarr[0]).equals("sendUDP"))
+				{
+					InetAddress address = InetAddress.getLocalHost();
+					new UDP(port, address);
+					UDP.send(filename);
+					testclient.close();
+					return;
+				}
+				if ((commandarr[0]).equals("sendTCP"))
+				{
+					new TCP();
+					TCP.sendfileTCP(filename, client);
+					testclient.close();
+					return;
+				}
 			}
 			testclient.close();
 		}
